6f866822846e3a3860f3e8c6cc3c7e9a5f498e0c
James Shvarts <jshvarts@gmail.com>
2017-03-23 09:39:36 -0400

    55: set up default values and remove with()

--- app/src/main/kotlin/com/exallium/todoapp/entities/Note.kt
+++ app/src/main/kotlin/com/exallium/todoapp/entities/Note.kt
@@ -3,20 +3,14 @@ package com.exallium.todoapp.entities
 /**
  * The Note entity
  */
-data class Note(val id: String,
-                val title: String,
-                val body: String,
-                val created: Long,
-                val updated: Long) {
+data class Note(val id: String = EMPTY_STRING,
+                val title: String = EMPTY_STRING,
+                val body: String = EMPTY_STRING,
+                val created: Long = EMPTY_TIMESTAMP,
+                val updated: Long = EMPTY_TIMESTAMP) {
 
-    fun with(id: String? = null,
-             title: String? = null,
-             body: String? = null,
-             created: Long? = null,
-             updated: Long? = null) = Note(
-            id ?: this.id,
-            title ?: this.title,
-            body ?: this.body,
-            created ?: this.created,
-            updated ?: this.updated)
+    companion object {
+        val EMPTY_STRING = ""
+        val EMPTY_TIMESTAMP = 0L
+    }
 }
